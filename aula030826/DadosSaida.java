cdd791410b2543493cd183be170d47e5d81d22a9
Vinicius de Araujo dos Anjos <vanjos990@gmail.com>
2026-08-05 22:06:15 -0300

    Create DadosSaida.java

---
+++ aula030826/DadosSaida.java
@@ -0,0 +1,60 @@
+package com.poo.prj_calculadoraeqseggrau_poo1.models;
+
+
+public class DadosSaida {
+    private double delta;
+    private double x1L;
+    private double x2L;
+    private double xV;
+    private double yV;
+
+    public double getDelta() 
+    {
+        return delta;
+    }
+
+    public void setDelta(double delta) 
+    {
+        this.delta = delta;
+    }
+
+    public double getX1L() 
+    {
+        return x1L;
+    }
+
+    public void setX1L(double x1L) 
+    {
+        this.x1L = x1L;
+    }
+
+    public double getX2L() 
+    {
+        return x2L;
+    }
+
+    public void setX2L(double x2L) 
+    {
+        this.x2L = x2L;
+    }
+
+    public double getxV() 
+    {
+        return xV;
+    }
+
+    public void setxV(double xV) 
+    {
+        this.xV = xV;
+    }
+
+    public double getyV() 
+    {
+        return yV;
+    }
+
+    public void setyV(double yV) 
+    {
+        this.yV = yV;
+    }    
+}

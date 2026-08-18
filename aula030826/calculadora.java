23c0e88a6754d1b45b2d5e206ec67d1c80a7bf20
Vinicius de Araujo dos Anjos <vanjos990@gmail.com>
2026-08-17 21:53:33 -0300

    Modify input to accept user values for A, B, C
    Updated input method to read values from user.

--- aula030826/calculadora.java
+++ aula030826/calculadora.java
@@ -3,14 +3,21 @@ package com.poo.prj_calculadoraeqseggrau_poo1;
 import com.poo.prj_calculadoraeqseggrau_poo1.bo.CalculoEqSegGrau;
 import com.poo.prj_calculadoraeqseggrau_poo1.models.DadosEntrada;
 import com.poo.prj_calculadoraeqseggrau_poo1.models.DadosSaida;
+import java.util.Scanner;
 
 public class Prj_CalculadoraEqSegGrau_POO1 {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
         DadosEntrada de = new DadosEntrada();
-        de.setA(1);
-        de.setB(5);
-        de.setC(6);
+        
+        System.out.println("Digite um n: ");
+        de.setA(Double.parseDouble(sc.nextLine()));
+        System.out.println("Digite um n: ");
+        de.setB(Double.parseDouble(sc.nextLine()));
+        System.out.println("Digite um n: ");
+        de.setC(Double.parseDouble(sc.nextLine()));
         
         CalculoEqSegGrau esg = new CalculoEqSegGrau();
         DadosSaida ds = esg.calcularDelta(de);
